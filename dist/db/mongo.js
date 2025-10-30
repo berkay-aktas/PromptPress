3a507de1a7f5395dcfe5cec3a58c700dbf98c7a1
ilysmbidkhtty <berkayaktas1915@gmail.com>
2025-10-30 21:34:09 +0300

    chore: remove node_modules and dist from repo tracking

--- dist/db/mongo.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-export async function connectMongo(uri) {
-    mongoose.set("strictQuery", true);
-    await mongoose.connect(uri);
-    console.log("✅ MongoDB connected");
-    return mongoose.connection;
-}
